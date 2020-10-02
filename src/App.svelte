80a88a2c370ded738ce56dc23cc2298f1c4fae60
vigenere23 <lolgab1@hotmail.com>
2020-10-02 09:42:55 -0400

    more complete setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,18 @@
 <script lang="ts">
-	export let name: string;
+	import { Component } from '@gabio/design-svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<Component></Component>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
