d1a91f8b36b2542eac5dc66bdc217dd1b3a6b4ad
vigenere23 <lolgab1@hotmail.com>
2020-10-05 11:31:07 -0400

    added dark theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <main>
-	<GioTitle centered>Hello, world!</GioTitle>
-	<GioText>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</GioText>
-	<GioBodyText>Some body told <GioInlineCode>me there waaas an</GioInlineCode> empty street somewhere that I didn't want to jaaaaaaaam...</GioBodyText>
-	<GioHeading level={1}>Yooo bros!</GioHeading>
-	<GioHeading level={2}>Yooo bros!</GioHeading>
-	<GioHeading level={3}>Yooo bros!</GioHeading>
-	<GioHeading level={4}>Yooo bros!</GioHeading>
-	<GioCaption>Click here for more!</GioCaption>
+	<GioWebPage headerHeight="0px">
+		<GioSection {dark}>
+			<GioContent>
+				<GioTitle {dark} centered>Hello, world!</GioTitle>
+				<GioSubtitle {dark}>Como estas?</GioSubtitle>
+				<GioText {dark}>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to <GioTag {dark}>learn</GioTag><GioTag {dark}>learn</GioTag> how to build Svelte apps.</GioText>
+				<GioBodyText {dark}>Some body told <GioInlineCode {dark}>me there waaas an</GioInlineCode> empty street somewhere that I didn't want to jaaaaaaaam...</GioBodyText>
+				<GioCaption {dark}>Click here for more!</GioCaption>
+				<GioHeading {dark} level={1}>Yooo bros!</GioHeading>
+				<GioHeading {dark} level={2}>Yooo bros! <GioTag {dark}>Svelte</GioTag></GioHeading>
+				<GioHeading {dark} level={3}>Yooo bros!</GioHeading>
+				<GioHeading {dark} level={4}>Yooo bros!</GioHeading>
+			</GioContent>
+		</GioSection>
+	</GioWebPage>
 </main>
 
 <script lang="ts">
@@ -16,8 +23,15 @@
 		GioBodyText,
 		GioHeading,
 		GioCaption,
-		GioInlineCode
+		GioInlineCode,
+		GioSection,
+		GioContent,
+		GioTag,
+		GioSubtitle,
+		GioWebPage
 	} from '@gabio/design-svelte'
+
+	const dark = false
 </script>
 
 <style lang="scss">
