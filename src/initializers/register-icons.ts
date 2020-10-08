073db3d7ff36a4db4462f3373134c11c0afe0a22
vigenere23 <lolgab1@hotmail.com>
2020-10-08 17:46:50 -0400

    updated lib

--- src/initializers/register-icons.ts
+++ src/initializers/register-icons.ts
@@ -1,4 +1,4 @@
-import { FaIconRegistry } from '@gabio/design-svelte/src/lib/faicon-registry'
+import { IconRegistry } from '@gabio/design-svelte/src/lib/icon-registry'
 import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
 import { faBackward } from '@fortawesome/free-solid-svg-icons/faBackward'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt'
@@ -9,7 +9,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
 import { faBehance } from '@fortawesome/free-brands-svg-icons/faBehance'
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn'
 
-FaIconRegistry.registerIcons({
+IconRegistry.registerIcons({
   faHome,
   faBackward,
   faExternalLinkAlt,
